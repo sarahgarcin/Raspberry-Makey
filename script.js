da8c808cdc7ed4421142f8c8b9dd727a44cabab5
sarahgarcin <sarahgarcin@hotmail.com>
2014-09-12 16:20:21 +0200

    Key print

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
 $(document).ready(function() {
-	
-	// $(".square").hide();
-		$(document).keydown(function(e){
-			if(e.keyCode == 65) {
-				$(".square").css("opacity", "0");
-				$(document).keyup(function(e){
-					if(e.keyCode == 65) {
-						$(".square").css("opacity", "1");
-					}
-				});
-			}
-		});
+	$(document).keydown(function(e){
+			console.log(e.keyCode, " is keydown");
+			$(".key-"+e.keyCode).css("opacity", "0");
 
-		$(document).keydown(function(e){
-			if(e.keyCode == 83) {
-				$(".rond").css("opacity", "0");
-				$(document).keyup(function(e){
-					if(e.keyCode == 83) {
-						$(".rond").css("opacity", "1");
-					}
-				});
-			}
-		});
-
-
-
+			$(document).keyup(function(e){
+				console.log(e.keyCode, " is up");
+				$(".key-"+e.keyCode).css("opacity", "1");
+			});
+	});
 });
